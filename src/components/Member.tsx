81d751133e93da11a7b31e7593134c437e64c4bd
Aniket Tamrakar <anikettamrakar0@gmail.com>
2024-08-02 10:49:05 +0545

    moreMembersCard component

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -1,12 +1,14 @@
 import { UserData } from "@/types/userdata.type";
 import Image from "next/image";
 import { useState } from "react";
+import MoreMembersCard from "./moreMembersCard";
 
 export default function Member({ member }: { member: UserData[] }) {
   const [showMembers, setShowMembers] = useState<boolean>(false);
   const showFullMembers = () => {
     setShowMembers(!showMembers);
   };
+
   return (
     <>
       <h1 className="font-semibold text-sm">Members</h1>
@@ -30,20 +32,7 @@ export default function Member({ member }: { member: UserData[] }) {
           </span>
         )}
         {showMembers && (
-          <div className="absolute right-2 px-4 py-3 mt-12 w-28 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-            <div className="flex">
-              <h1>Members</h1>
-              
-            </div>
-            {/* <ul className="">
-              <li className="w-full px-3 py-1 text-xs hover:bg-gray-400 cursor-pointer rounded-md">
-                Edit
-              </li>
-              <li className="w-full px-3 py-1 text-xs hover:bg-gray-400 cursor-pointer rounded-md">
-                Delete
-              </li>
-            </ul> */}
-          </div>
+        <MoreMembersCard member={member} setShowMembers={setShowMembers} showMembers={showMembers}/>
         )}
       </div>
 
