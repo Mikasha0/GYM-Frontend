4a7e1b279a4ca5b172b41297ab5a7c5c711df8ca
Aniket Tamrakar <anikettamrakar0@gmail.com>
2024-08-17 19:40:27 +0545

    changed date time

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,34 +1,44 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
-export const fetchTime = async () => {
-  const response = await fetch("https://corsproxy.io/?https://worldtimeapi.org/api/ip");
-  if (!response.ok) {
-    throw new Error("Failed to fetch time");
-  }
-  return response.json();
-};
+// export const fetchTime = async () => {
+//   const response = await fetch("https://corsproxy.io/?https://worldtimeapi.org/api/ip");
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch time");
+//   }
+//   return response.json();
+// };
 
 const Greeting = () => {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["greeting"],
-    queryFn: fetchTime,
-  });
+  // const { data, error, isLoading } = useQuery({
+  //   queryKey: ["greeting"],
+  //   queryFn: fetchTime,
+  // });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error fetching time</div>;
+  // if (isLoading) return <div>Loading...</div>;
+  // if (error) return <div>Error fetching time</div>;
 
-  const currentHour = new Date(data.datetime).getHours();
+  // const currentHour = new Date(data.datetime).getHours();
 
+  // let greeting;
+  // if (currentHour < 12) {
+  //   greeting = "Good Morning 🌞";
+  // } else if (currentHour < 18) {
+  //   greeting = "Good Afternoon 👋🌞";
+  // } else {
+  //   greeting = "Good Evening 🌛";
+  // }
+  var today = new Date();
+  var curHr = today.getHours();
   let greeting;
-  if (currentHour < 12) {
+  if (curHr < 12) {
     greeting = "Good Morning 🌞";
-  } else if (currentHour < 18) {
+  } else if (curHr < 18) {
     greeting = "Good Afternoon 👋🌞";
   } else {
     greeting = "Good Evening 🌛";
   }
-
+  console.log(greeting);
   return (
     <div>
       <h1>{greeting} , Aniket</h1>
